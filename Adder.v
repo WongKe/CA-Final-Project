301a90334d8ccaecc27972d1d81fe72862c34939
jonm2012 <jonm2012@gmail.com>
2017-04-23 12:54:40 -0400

    Add files via upload

---
+++ Adder.v
@@ -0,0 +1,16 @@
+//EECE3324
+//Computer Architecture
+//Final Project
+//Kevin Wong & Jonathan Marrero-Rosaly
+
+
+// Adder 32-bits
+
+module Adder(input0, input1, output0);
+  input [31:0] input0, input1;
+  output reg [31:0] output0;
+  
+  always @ (input0 or input1) begin
+    output0 = input0 + input1;
+  end
+endmodule
