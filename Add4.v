d1574e4c5063bd3dc9573f9303a82db4bec483e9
jonm2012 <jonm2012@gmail.com>
2017-04-23 12:50:56 -0400

    Add files via upload

---
+++ Add4.v
@@ -0,0 +1,17 @@
+//EECE3324
+//Computer Architecture
+//Final Project
+//Kevin Wong & Jonathan Marrero-Rosaly
+
+
+// Adds 4 to input
+
+module Add4(innput0, output0);
+  input [31:0] input0;
+  output reg [31:0] output0;
+  
+  always @ (input0) begin
+    output0 = input0 + 4;
+  end
+  
+endmodule
