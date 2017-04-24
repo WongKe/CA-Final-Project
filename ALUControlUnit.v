42d8a2bc863cb78151f9c881a5030a9a015e13a9
jonm2012 <jonm2012@gmail.com>
2017-04-24 17:26:48 -0400

    Update ALUControlUnit.v

--- ALUControlUnit.v
+++ ALUControlUnit.v
@@ -7,22 +7,63 @@
 
 //Taken from HW6 Solutions
 
-module ALUControlUnit(ALUOPcode, ALUOP,FunctCode);
-input [2:0] ALUOP;
-input [5:0] FunctCode;
-output reg [3:0] ALUOPcode;
+`timescale 1ns / 1ns
 
-always @(ALUOP or FunctCode)
-begin
-
-if(ALUOP==3'b100) ALUOPcode <= 4'b0010;    //lw,store,addi
-else if(ALUOP==3'b111) ALUOPcode <= 4'b0110;  //beq
-else if(ALUOP==3'b101) ALUOPcode <= 4'b0000;  //andi
-else if(ALUOP==3'b110) ALUOPcode <= 4'b0001;  //ori
-else if(ALUOP==3'b010&& FunctCode==6'b100000) ALUOPcode <= 4'b0010;//add
-else if(ALUOP==3'b010&& FunctCode==6'b100010) ALUOPcode <= 4'b0110;//sub
-else if(ALUOP==3'b010&& FunctCode==6'b100100) ALUOPcode <= 4'b0000;//and
-else if(ALUOP==3'b010&& FunctCode==6'b101010) ALUOPcode <= 4'b0111;//slt
-else if(ALUOP==3'b010&& FunctCode==6'b100101) ALUOPcode <= 4'b0111;//or
-end
-endmodule 
+module ALUControlUnit(ALUop, addi, func, ALUoperation);
+  
+  input [1:0] ALUop;
+  input addi;
+  input [5:0] func;
+  output reg [3:0] ALUoperation;
+  
+  // initialize
+  initial begin
+    ALUoperation = 4'b0000;
+  end
+  
+  always @ (ALUop or func) begin
+    
+    case (ALUop)
+      
+      // lw & sw (add)
+      2'b00: begin ALUoperation = 4'b0010; end
+      
+      // beq (sub)
+      2'b01: begin ALUoperation = 4'b0110; end
+      
+      // ALUOp1 == 1
+      2'b10: begin
+        // allow for addi
+        case (addi)
+          1'b1: begin ALUoperation = 4'b0010; end
+          
+          1'b0: begin
+            case (func[3:0]) // func[5:4] is don't care
+              // add
+              4'b0000: begin ALUoperation = 4'b0010; end
+              // sub
+              4'b0010: begin ALUoperation = 4'b0110; end
+              // and
+              4'b0100: begin ALUoperation = 4'b0000; end
+              // or
+              4'b0101: begin ALUoperation = 4'b0001; end
+              // slt
+              4'b1010: begin ALUoperation = 4'b0111; end
+            endcase
+          end
+        endcase
+      end
+      
+      // handle alternate don't care cases
+      2'b11: begin
+        case (func[3:0]) // func[5:4] is don't care
+          // sub
+          4'b0010: begin ALUoperation = 4'b0110; end
+          // slt
+          4'b1010: begin ALUoperation = 4'b0111; end
+        endcase
+      end
+    endcase
+  end
+  
+endmodule
